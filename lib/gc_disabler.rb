2e842e0209dbb1350f02bb67e3490b5de9ffd859
David Moore <davidimoore@gmail.com>
2017-06-24 11:00:24 -0700

    GC disabler class

---
+++ lib/gc_disabler.rb
@@ -0,0 +1,12 @@
+class GCDisabler
+  def initialize(app)
+    @app = app
+  end
+  def call(env)
+    GC.start
+    GC.disable
+    response = @app.call(env)
+    GC.enable
+    response
+  end
+end
