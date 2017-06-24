c064b9e3070ab55e290fb1c9c8da7b65fbd5019e
David Moore <davidimoore@gmail.com>
2017-06-24 11:01:18 -0700

    adds user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,30 @@
 # migrate things
-Thing.delete_all
-Minion.delete_all
-
-
+[User, Thing, Minion].each(&:delete_all)
 
 def attribute_set(columns, val)
   columns.inject({}) {|h, attr| h[attr] = val; h  }
 end
-val = "x" * 100
-columns  = Thing.column_names - ["id", "created_at", "updated_at"]
 
-10000.times do |i|
-  Thing.create(attribute_set(columns, val))
+def seed_things
+  val = "x" * 100
+  columns  = Thing.column_names - ["id", "created_at", "updated_at"]
+
+  10000.times do |i|
+    Thing.create(attribute_set(columns, val))
+  end
 end
+
+
+def seed_users(quantity)
+  quantity.times do
+    User.create(
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            email: Faker::Internet.email
+    )
+  end
+end
+
+seed_users(10)
+
+
